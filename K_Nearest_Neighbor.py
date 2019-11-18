c042600e48726cd80bd8d2e0357d5b0edf74136a
Bar Movshovich <barmovshovich@Bars-MacBook-Pro.local>
2019-11-17 20:27:33 -0800

    slightly working version of KNN

--- K_Nearest_Neighbor.py
+++ K_Nearest_Neighbor.py
@@ -11,6 +11,7 @@ def main():
     parser = argparse.ArgumentParser(description='figure out heart anomalys')
     parser.add_argument('--train', '-tr', type=str, default=None, help='train file')
     parser.add_argument('--test', '-te', type=str, default=None, help='test file')
+    parser.add_argument('--k', '-k', type=int, default=1, help='length of k for KNN')
 
     args = parser.parse_args()
     if args.train == None or args.test == None:
@@ -18,6 +19,10 @@ def main():
         print("no input file detected")
         return -1
     raw_data = None
+    if args.k % 2 == 0:
+        k = args.k + 1
+    else:
+        k = args.k
     with open(args.train, 'r') as f: 
         raw_data = [[int(num) for num in line.split(",")] for line in f]
         print("raw_data of file is: \n", np.matrix(raw_data))
@@ -47,30 +52,6 @@ def main():
     classify = raw_data[:, [0]]
     # print("classify is: \n", classify)
 
-    # set default for abnormal and normal totals
-    abnormal = 0
-    normal = 0
-
-    # loop through the number of rows we have. 
-    for i in range(rows):
-        # check the first row's binary. If true increment normal else increment abnormal. 
-        if classify[i] == 1:
-            normal += 1
-        elif classify[i] == 0:
-            abnormal += 1
-
-    # adding 0.5 to probability to prevent log issue later on. 
-    P0_prob = (abnormal + 0.5)/(rows + 0.5)
-    P1_prob = (normal + 0.5)/(rows + 0.5)
-
-    # print out the number of normal and abnormal as well as 
-    # the probability of each. 
-    print("normal is: ", normal)
-    print("abnormal is: ", abnormal)
-    print("P0_prob is: ", P0_prob)
-    print("P1_prob is: ", P1_prob)
-
-
     # Now that we have P(0) and P(1) we now need to find the probability of each column. 
     # we start at the 2nd column. 
     for i in range(column_length):
@@ -83,36 +64,43 @@ def main():
         # expect normal, get normal
         temp_P1_1 = 0
 
-
         # grab the ith column +1 to avoid 1st column
         column = raw_data[:, [i+1]]
         # print("other column is: ", column) 
 
-        # Loop through every row and check if the test is a 1 or 0. 
+        # Loop through every row of length "k" and check if the test is a 1 or 0. 
         for j in range(rows):
-            if column[j] == 1:
-                if classify[j] == 1: 
-                    temp_P1_1 += 1
-                else: 
-                    temp_P1_0 += 1
+            # loop through K nearest neighbors.
+            for d in range(k):
+                # make sure we don't go out of bounds.
+                if j <= (rows-k):
+                    if column[j+d] == 1:
+                        if classify[j+d] == 1: 
+                            temp_P1_1 += 1
+                        else: 
+                            temp_P1_0 += 1
 
-            elif column[j] == 0:
-                if classify[j] == 1: 
-                    temp_P0_1 += 1
-                else: 
-                    temp_P0_0 += 1
+                    elif column[j+d] == 0:
+                        if classify[j+d] == 1: 
+                            temp_P0_1 += 1
+                        else: 
+                            temp_P0_0 += 1
 
-        # add the probability of 0 to P0 at index i
-        P0_0[i] = np.log2((temp_P0_0 + 0.5)/(abnormal + 0.5))
-        P1_0[i] = np.log2((temp_P1_0 + 0.5)/(abnormal + 0.5))
-        P1_1[i] = np.log2((temp_P1_1 + 0.5)/(normal + 0.5))
-        P0_1[i] = np.log2((temp_P0_1 + 0.5)/(normal + 0.5))
+        # get the KNN distance for each probability.
+        P0_0[i] = temp_P0_0
+        P1_0[i] = temp_P1_0
+        P1_1[i] = temp_P1_1
+        P0_1[i] = temp_P0_1
+        temp_P0_0 = 0
+        temp_P0_1 = 0
+        temp_P1_0 = 0
+        temp_P1_1 = 0
 
     # probability based off of training data.
-    # print("P0_0 is \n", P0_0)
-    # print("P1_0 is \n", P1_0)
-    # print("P1_1 is \n", P1_1)
-    # print("P0_1 is \n", P0_1)
+    print("P0_0 is \n", P0_0)
+    print("P1_0 is \n", P1_0)
+    print("P1_1 is \n", P1_1)
+    print("P0_1 is \n", P0_1)
 
     # open up test data and save to array called test_data 
     with open(args.test, 'r') as k: 
@@ -128,9 +116,9 @@ def main():
     print("test_rows_length is: ", test_rows_length)
 
     # Final result.
-    Naive_guess = np.zeros((test_rows_length))
+    KNN_guess = np.zeros((test_rows_length))
 
-    # Loop through all test_rows
+    # Loop through all test_rows for length of "k"
     for j in range(test_rows_length):
         test_rows = test_data[[j],:]
         #print("test_rows is: ", test_rows)
@@ -141,24 +129,23 @@ def main():
             # we add 1 to skip first column
             # print("i is: ", i)
             if test_rows[0][i+1] == 0:
-                P_AB += P0_0[i]
-                P_N += P0_1[i]
+                P_AB += P0_0[i]/(P0_0[i] + P0_1[i])
+                P_N += P0_1[i]/(P0_1[i] + P0_0[i])
 
             # we add 1 to skip first column
             elif test_rows[0][i+1] == 1:
+                P_AB += P1_0[i]/(P1_0[i] + P1_1[i])
+                P_N += P1_1[i]/(P1_1[i] + P1_0[i])
 
-                P_AB += P1_0[i]
-                P_N += P1_1[i]
 
-        P_AB += P0_prob
-        P_N += P1_prob
-
+        # print("P_AB is: \n", P_AB)
+        # print("P_N is: \n", P_N)
         if P_AB > P_N:
-            Naive_guess[j] = 0
+            KNN_guess[j] = 0
         else:
-            Naive_guess[j] = 1
+            KNN_guess[j] = 1
 
-    print("Naive_guess is: \n", Naive_guess)
+    print("KNN_guess is: \n", KNN_guess)
 
     test_column = test_data[:, [0]]
     # print("test_column is: \n", test_column)
@@ -174,10 +161,10 @@ def main():
             abnormal_count += 1
         if (test_column[i] == 1):
             normal_count += 1
-        if (test_column[i] == 1 and Naive_guess[i] == 1):
+        if (test_column[i] == 1 and KNN_guess[i] == 1):
             classified_correctly += 1
             normal_correctly += 1
-        elif (test_column[i] == 0 and Naive_guess[i] == 0):
+        elif (test_column[i] == 0 and KNN_guess[i] == 0):
             classified_correctly += 1
             abnormal_correctly += 1
 
